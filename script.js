6c5cb22c9b68ae833683b98ec802d57b6254e201
MacreenSmith <macreensmith@gmail.com>
2023-06-22 13:03:15 -0400

    added text, appends, and lists

---
+++ script.js
@@ -0,0 +1,74 @@
+// bring in elements from todo list
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todoUL = document.getElementById("todos");
+const todos = JSON.parse(localStorage.getItem("todos"))
+console.log(todoUL);
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //console.log(e)
+
+    addTodo();
+});
+
+function addTodo(todo) {
+    //saving the input value (text) to a variable
+    let todoText = input.value
+    //checking if a todo exists
+    if (todo) {
+        //set the value of input to the value of todo.text
+        todoText = todo.todoText
+    }
+    //if the text exists
+    if (todoText) {
+        //create a new list item
+        const todoEL = document.createElement('li');
+        //if there is a todo and its completed
+        if (todo && todo.completed) {
+            //add the strikethrough class
+            todoEL.classList.add('completed')
+        }
+        //make the text of li same as input value
+        todoEL.innerText = todoText
+        //append the todo li item to the todo unordered list
+        todoUL.appendChild(todoEL)
+        //clear out after enter
+        input.value = " "
+
+        todoEL.addEventListener('click', () => {
+            todoEL.classList.toggle('completed');
+            updateLS();
+
+            todoEL.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
+                //remove list item
+                todoEL.remove()
+                updateLS()
+            })
+        });
+    }
+    
+    updateLS()
+}
+
+function updateLS() {
+    const todosEL = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEL.forEach((todoEL) => {
+        todos.push({
+            text: todoEL.innerText,
+            completed: todoEL.classList.contains('completed'),
+        })
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
